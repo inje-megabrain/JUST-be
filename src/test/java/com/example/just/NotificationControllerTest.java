bf5202a6a23fcabafe697d1f0c2d5ecbb6dee73f
sleeg00 <slee000220@gmail.com>
2023-03-20 14:19:57 +0900

    JUST 폴더 내 파일 루트 폴더로 이동

--- src/test/java/com/example/just/NotificationControllerTest.java
+++ src/test/java/com/example/just/NotificationControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.just;
 
+/*
 import com.example.just.Controller.NotificationController;
 import com.example.just.Dao.Member;
 import com.example.just.Repository.MemberRepository;
@@ -53,3 +54,5 @@ public class NotificationControllerTest {
         mockMvc.perform(get("/noti").header("X-AUTH-TOKEN",access)).andExpect(status().isOk());
     }
 }
+
+ */
