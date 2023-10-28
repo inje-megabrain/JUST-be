40072a2ce83ce74f110176b8b7b0e7d276b84f8f
sleeg00 <slee000220@gmail.com>
2023-10-28 16:36:48 +0900

    Refector: Post API

--- src/main/java/com/example/just/Mapper/PostMapper.java
+++ src/main/java/com/example/just/Mapper/PostMapper.java
@@ -2,8 +2,10 @@ package com.example.just.Mapper;
 
 import com.example.just.Dao.Post;
 import com.example.just.Dto.PostDto;
+import com.example.just.Dto.PostPostDto;
+import com.example.just.Dto.PutPostDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper extends EntityMapper<PostDto, Post>{
+public interface PostMapper extends EntityMapper<PutPostDto, Post>{
 }
